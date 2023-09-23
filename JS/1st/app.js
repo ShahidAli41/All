f89dcbc58d3618eef3d44015d70120b768593d30
ShahidAli41 <fazanequran41@gmail.com>
2023-09-23 16:07:04 +0500

    js

---
+++ JS/1st/app.js
@@ -0,0 +1,17 @@
+document.write("<h2>(abusaad) Shahid Ali</h2>")
+
+alert("Wellcome to Js first Claas  Alert");
+console.log("This Example Off console")
+alert("this is line brack Example \n 1st line \n 2nd line \n 3rd line \n 4th line")
+alert("My Email-Address is faizanequran41@gmail.com")
+alert("My Mobile No is 0313-2092639")
+
+
+prompt("pleas Enter Your Ful Name")
+var Name = "CCO-117323"
+var Example2 = "this is Variable alart Example"
+var Example = "spacial thanks for sir Rizwan Bhatti & Saylani SMIT"
+
+document.write(Name)
+alert(Example2)
+alert(Example)

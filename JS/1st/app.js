c71c15063d3b65cc5aa0a6f566608ee92e1dbb35
ShahidAli41 <fazanequran41@gmail.com>
2023-09-25 08:53:57 +0500

    js

--- JS/1st/app.js
+++ JS/1st/app.js
@@ -4,7 +4,7 @@ alert("Wellcome to Js first Claas  Alert");
 console.log("This Example Off console")
 alert("this is line brack Example \n 1st line \n 2nd line \n 3rd line \n 4th line")
 alert("My Email-Address is faizanequran41@gmail.com")
-alert("My Mobile No is 0313-2092639")
+// alert("My Mobile No is 0313-2092639")
 
 
 prompt("pleas Enter Your Ful Name")
